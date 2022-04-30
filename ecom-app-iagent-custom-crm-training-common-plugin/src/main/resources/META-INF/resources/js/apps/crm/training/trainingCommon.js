1b3ee52fa9b5fd5c21ebe67740a53d1ef4a32c07
octohindol <hindol@octopus8.com>
2022-04-30 04:00:45 +0300

    have access to API checked-can check phone-3

---
+++ ecom-app-iagent-custom-crm-training-common-plugin/src/main/resources/META-INF/resources/js/apps/crm/training/trainingCommon.js
@@ -0,0 +1,5 @@
+$(".popUplink").click(function(){
+    var hyperLink = $(this).attr('href');
+    console.log(hyperLink);
+    window.open(hyperLink, 'popup', 'width=800,height=800'); return false;
+});
